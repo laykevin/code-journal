0e6f79ccf093c11d5376c803fbadcedfa48c390a
Kevin Lay <kevinlay@live.com>
2023-02-14 00:07:42 +0000

    Added a conditional statement that checks if a JSON data model is stored in localStorage, and pases it to the data variable if there is.

--- js/data.js
+++ js/data.js
@@ -12,3 +12,8 @@ function serializeData(event) {
   var dataJSON = JSON.stringify(data);
   localStorage.setItem('code-journal-local-storage', dataJSON);
 }
+
+var previousDataJSON = localStorage.getItem('code-journal-local-storage');
+if (previousDataJSON !== null) {
+  data = JSON.parse(previousDataJSON);
+}
